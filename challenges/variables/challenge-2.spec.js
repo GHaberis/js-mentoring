98deca61bf0649356202dcdde965bee7602718b7
Francesca Hammond <francesca.hammond@guardian.co.uk>
2018-06-19 15:37:03 +0100

    Answers to challeng 1 and 2

--- challenges/variables/challenge-2.spec.js
+++ challenges/variables/challenge-2.spec.js
@@ -2,7 +2,7 @@ import challenge from "./challenge-2";
 
 describe("Challenge #2: Returns a sentence", () => {
   it("should return a fully formed sentence", () => {
-    const sentence = challenge('Helen', 'Software engineer');
+    const sentence = challenge('Helen', 'software engineer');
 
     expect(sentence).toEqual("Hello I am Helen, and I am a software engineer");
   });
