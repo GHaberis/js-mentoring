abb64e82f3f4830315a6873934720e7f0fd18b70
GHaberis <george.haberis@theguardian.com>
2018-10-25 12:07:00 +0100

    new challenge

---
+++ challenges/6. recap/challenge-17.js
@@ -0,0 +1,25 @@
+/**
+  You have just opened a clothes shop. The inventory for your shop is:
+
+  - (£100) Jeans x 2
+  - (£60) Shirts x 2
+  - (£25) T-shirts x 5
+  - (£5) Socks x 5
+  - (£50) Sunglasses x 10
+
+  All prices listed do not have VAT added. VAT is 20%.
+
+  Requirements
+  1. Write a function which returns the total value of your stock (VAT not included)
+  2. Write a function which takes an order and returns the total value of that order including VAT.
+  3. You're also planning a 30% off sale for Jeans and Socks! Make sure the function that returns an orders total takes this into account when a sale is on.
+*/
+
+/**
+  Advanced:
+
+  - Create a shopping cart that allows customers to add and remove items of clothing
+  - When a user succesfully adds an item to their cart a message returns "your cart total is £X"
+  - When a user succesfully adds an item to their cart a message returns "your cart total is £X"
+  - If a user tries to add an item that's out of stick the cart should prevent this and return a message saying "sorry, this item is unavailable"
+*/
